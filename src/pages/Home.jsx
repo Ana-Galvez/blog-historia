47730ff96e55a75d6b58ef28acb736653323a02f
Ana-Galvez <anamariagalvez01@gmail.com>
2025-11-23 12:23:33 -0300

    fix: Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
       <h1>Bienvenido al Blog de Minibiografías Históricas</h1>
       <p>Explora las diferentes eras de la historia de la humanidad a través del menú.</p>
       <p>Desde los albores de la civilización hasta el mundo moderno, descubre los eventos y las personas que formaron nuestra sociedad.</p>
+      <p>Si quieres saber más sobre mí, puedes visitar mi <a href="https://www.youtube.com/c/Minibiograf%C3%ADashist%C3%B3ricas" target="_blank">canal de YouTube</a>.</p>
     </div>
   );
 };
