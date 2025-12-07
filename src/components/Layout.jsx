92fc2bfe621baaa2bd73db9a31feb839c671068a
Ana-Galvez <anamariagalvez01@gmail.com>
2025-12-07 03:07:43 -0300

    feat: agregado cafecito

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,6 +8,7 @@ const Layout = () => {
     <div className="app-container">
       <Sidebar />
       <main className="main-content">
+<a href='https://cafecito.app/minibiografias1688' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_2.png 1x, https://cdn.cafecito.app/imgs/buttons/button_2_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_2_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_2.png' alt='Invitame un café en cafecito.app' /></a>
         <Outlet />
       </main>
       <Footer />
